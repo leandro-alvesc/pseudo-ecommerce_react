525dc858a529b52bef5baf728db8bf10e60e7c9f
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-02-26 18:19:54 -0300

    att links e css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={ProductList} />
-        <Route path="/details" component={Details} />
+        {/*<Route path="/details" component={Details} />*/}
         <Route path="/cart" component={Cart} />
         <Route component={Default} />
       </Switch>
